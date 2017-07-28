105561e58ae157f6308b9e04d92cb673b1f3aff7
Hunter Warren <hunter.warren@gmail.com>
2017-07-28 09:47:49 -0500

    started working on assessment!

--- assessment.php
+++ assessment.php
@@ -1,3 +1,47 @@
 <?php
 
 // Write your code here
+
+// isNegative ////////////////////////////////////////////////
+
+
+// function isNegative($number)
+// {
+//   if (is_numeric($number) !== false) {
+//     if ($number < 0)
+//     {
+//       return true;
+//     } else if ($number > 0)
+//     {
+//       return false;
+//     }
+//   } else {
+//     return NULL;
+//   }
+// }
+
+// average  //////////////////////////////////////////////////
+
+// count odds ////////////////////////////////////////////////
+
+
+function countOdds($array) {
+  if (is_array($array)){
+    $count = 0;
+    foreach($array as $key => $value) {
+      if (is_numeric($value)) {
+        if ($value % 2 !== 0) {
+          $count++;
+        } else {
+          continue;
+        }
+      } else if (defined($value) == false){
+        continue;
+      }
+
+    }
+    return $count.PHP_EOL;
+  } else {
+    return false;
+  }
+}
