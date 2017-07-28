6ce17b0965de5d44bd51da112e7a4c2e0e866ba8
Vivian Canales <viviancans@gmail.com>
2017-07-28 09:04:17 -0500

    initial commit for php assessment

--- assessment.php
+++ assessment.php
@@ -1,3 +1,52 @@
 <?php
 
-// Write your code here
+// Write a function named isNegative that accepts a number and returns true or false based on whether the input is negative.
+
+
+
+
+//Write a function named average that accepts an array of numbers and returns the average of those numbers
+
+
+
+
+
+
+
+//Write a function named countOdds that accepts an array of numbers and returns the number of odd numbers in the array.
+
+
+
+
+
+
+
+
+
+//Write a function named convertNameToAssociativeArray that accepts a string that contains a first name and last name separated by a space, and returns an associative array with keys of firstName and lastName.
+
+
+
+
+
+
+
+
+
+
+
+
+//Write a function named fiveTo that accepts a number and returns an array of the integers from 5 to (including) the passed number.
+
+
+
+
+
+
+
+
+
+
+
+
+//Write a function named upperCaseLastNames that accepts an array of associative arrays where each associative array has indices firstName and lastName and returns the same array of associative arrays with each last name capitalized
