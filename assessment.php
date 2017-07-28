c9c1469decef0557f2eb625e30e2029973870db0
Emmett Peters <ejp8611@gmail.com>
2017-07-28 09:15:11 -0500

    finished isNeg and average functions'

--- assessment.php
+++ assessment.php
@@ -1,3 +1,24 @@
 <?php
 
 // Write your code here
+function isNegative($var){
+	if (is_numeric($var) && $var < 0){
+		return true;
+	} else {
+		return false;
+	}
+};
+
+echo isNegative(-4);
+
+function average($var){
+	$total = 0;
+	foreach($var as $indiv){
+		$total+=$indiv;
+	}
+	return $total/count($var);
+};
+
+echo average([1,2,3,4,5]);
+
+ 
