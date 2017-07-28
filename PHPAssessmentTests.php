07ae1273dbab79ca8e6407220339bb4806a9b907
Cody <cody@hastings.email>
2017-07-28 10:59:05 -0500

    committing PHP assessment

--- PHPAssessmentTests.php
+++ PHPAssessmentTests.php
@@ -6,184 +6,184 @@ require ASSESSMENT_FILE;
 
 class PHPAssessmentTests extends PHPUnit_Framework_TestCase
 {
-    public function test_is_negative()
-    {
-        $negativeNumber = mt_rand(-200, -50);
-        $positiveNumber = mt_rand(20, 400);
+    // public function test_is_negative()
+    // {
+    //     $negativeNumber = mt_rand(-200, -50);
+    //     $positiveNumber = mt_rand(20, 400);
 
 
-        $this->assertInternalType(
-            'callable',
-            'isNegative',
-            'Expected isNegative() function to be present.'
-        );
+    //     $this->assertInternalType(
+    //         'callable',
+    //         'isNegative',
+    //         'Expected isNegative() function to be present.'
+    //     );
 
-        $this->assertInternalType(
-            'bool',
-            isNegative(12),
-            "Expected the isNegative(12) function to return a boolean"
-        );
+    //     $this->assertInternalType(
+    //         'bool',
+    //         isNegative(12),
+    //         "Expected the isNegative(12) function to return a boolean"
+    //     );
 
-        $this->assertInternalType(
-            'bool', 
-            isNegative(-12),
-            "Expected the isNegative(-12) function to return a boolean"
-        );
+    //     $this->assertInternalType(
+    //         'bool', 
+    //         isNegative(-12),
+    //         "Expected the isNegative(-12) function to return a boolean"
+    //     );
 
         
-        $this->assertTrue(
-            isNegative($negativeNumber),
-             "Expected isNegative($negativeNumber) to return true."
-        );
+    //     $this->assertTrue(
+    //         isNegative($negativeNumber),
+    //          "Expected isNegative($negativeNumber) to return true."
+    //     );
         
-        $this->assertFalse(
-            isNegative($positiveNumber),
-            "Expected isNegative($positiveNumber) to return false."
-        );
+    //     $this->assertFalse(
+    //         isNegative($positiveNumber),
+    //         "Expected isNegative($positiveNumber) to return false."
+    //     );
 
-        $this->assertTrue(
-            isNegative(-10), 
-            "Expected isNegative(-10) to return true"
-        );
+    //     $this->assertTrue(
+    //         isNegative(-10), 
+    //         "Expected isNegative(-10) to return true"
+    //     );
 
-        $this->assertFalse(
-            isNegative(123), 
-            "Expected isNegative(123) to return false"
-        );
+    //     $this->assertFalse(
+    //         isNegative(123), 
+    //         "Expected isNegative(123) to return false"
+    //     );
 
-        $this->assertFalse(
-            isNegative(0), 
-            "Expected isNegative(0) to return false."
-        );
-    }
+    //     $this->assertFalse(
+    //         isNegative(0), 
+    //         "Expected isNegative(0) to return false."
+    //     );
+    // }
 
-    public function test_average()
-    {
-        $this->assertInternalType(
-            'callable', 
-            'average',
-            "Expected average() function to be present."
-        );
+    // public function test_average()
+    // {
+    //     $this->assertInternalType(
+    //         'callable', 
+    //         'average',
+    //         "Expected average() function to be present."
+    //     );
 
-        $this->assertInternalType(
-            'numeric',
-            average([1, 2, 3]),
-            "Expected average() to return a numeric result"
-        );
+    //     $this->assertInternalType(
+    //         'numeric',
+    //         average([1, 2, 3]),
+    //         "Expected average() to return a numeric result"
+    //     );
 
-        $this->assertEquals(
-            1.5, average([1, 2]), 
-            "Expected average([1, 2]) to equal 1.5"
-        );
+    //     $this->assertEquals(
+    //         1.5, average([1, 2]), 
+    //         "Expected average([1, 2]) to equal 1.5"
+    //     );
 
-        $this->assertEquals(
-            2, average([1, 2, 3]), 
-            "Expected average([1, 2, 3]) to equal 2"
-        );
-    }
+    //     $this->assertEquals(
+    //         2, average([1, 2, 3]), 
+    //         "Expected average([1, 2, 3]) to equal 2"
+    //     );
+    // }
 
-    public function test_count_odds()
-    {
+    // public function test_count_odds()
+    // {
 
-        $this->assertInternalType(
-            'callable',
-            'countOdds',
-            'Expected countOdds() function to be present.'
-        );
+    //     $this->assertInternalType(
+    //         'callable',
+    //         'countOdds',
+    //         'Expected countOdds() function to be present.'
+    //     );
 
-        $this->assertInternalType(
-            'numeric',
-            countOdds([1, 2, 3]),
-            "Expected countOdds([1, 2, 3]) to return a numeric result."
-        );
+    //     $this->assertInternalType(
+    //         'numeric',
+    //         countOdds([1, 2, 3]),
+    //         "Expected countOdds([1, 2, 3]) to return a numeric result."
+    //     );
 
-        $this->assertEquals(
-            2, countOdds([1, 2, 3]), 
-            "Expected countOdds[1, 2, 3] to equal 2."
-        );
+    //     $this->assertEquals(
+    //         2, countOdds([1, 2, 3]), 
+    //         "Expected countOdds[1, 2, 3] to equal 2."
+    //     );
 
-        $this->assertEquals(
-            5, countOdds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-            "Expected countOdds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) to equal 5."
-        );
+    //     $this->assertEquals(
+    //         5, countOdds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+    //         "Expected countOdds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) to equal 5."
+    //     );
 
-        $this->assertEquals(
-            0, countOdds([0, 2, 4, 6]), 
-            "Expected countOdds([0, 2, 4, 6]) to equal 0"
-        );
-    }
+    //     $this->assertEquals(
+    //         0, countOdds([0, 2, 4, 6]), 
+    //         "Expected countOdds([0, 2, 4, 6]) to equal 0"
+    //     );
+    // }
 
-    public function test_convert_name_to_associative_array()
-    {
-        $this->assertInternalType(
-            'callable',
-            'convertNameToAssociativeArray',
-            "Expected convertNameToAssociativeArray() function to be present."
-        );
+    // public function test_convert_name_to_associative_array()
+    // {
+    //     $this->assertInternalType(
+    //         'callable',
+    //         'convertNameToAssociativeArray',
+    //         "Expected convertNameToAssociativeArray() function to be present."
+    //     );
 
-        $this->assertInternalType(
-            'array',
-            convertNameToAssociativeArray("John Dillinger"),
-            "Expected convertNameToAssociativeArray('John Dillinger') to return an array"
-        );
+    //     $this->assertInternalType(
+    //         'array',
+    //         convertNameToAssociativeArray("John Dillinger"),
+    //         "Expected convertNameToAssociativeArray('John Dillinger') to return an array"
+    //     );
 
-        $this->assertArrayHasKey('firstName',
-            convertNameToAssociativeArray('John Dillinger'), 
-            "Expected the array returned by convertNameToAssociativeArray('John Dillinger') to have 'firstName' as a defined index."
-        );
+    //     $this->assertArrayHasKey('firstName',
+    //         convertNameToAssociativeArray('John Dillinger'), 
+    //         "Expected the array returned by convertNameToAssociativeArray('John Dillinger') to have 'firstName' as a defined index."
+    //     );
 
-        $this->assertArrayHasKey('lastName',
-            convertNameToAssociativeArray('John Dillinger'),
-            "Expected the array returned by convertNameToAssociativeArray('John Dillinger') to have 'lastName' as a defined index."
-        );
+    //     $this->assertArrayHasKey('lastName',
+    //         convertNameToAssociativeArray('John Dillinger'),
+    //         "Expected the array returned by convertNameToAssociativeArray('John Dillinger') to have 'lastName' as a defined index."
+    //     );
 
-        $this->assertEquals(
-            ['firstName' => 'Al', 'lastName' => 'Capone'],
-            convertNameToAssociativeArray('Al Capone'),
-            "Expected convertNameToAssociativeArray('Al Capone') to equal ['firstName' => 'Al', 'lastName' => 'Capone']"
-        );
+    //     $this->assertEquals(
+    //         ['firstName' => 'Al', 'lastName' => 'Capone'],
+    //         convertNameToAssociativeArray('Al Capone'),
+    //         "Expected convertNameToAssociativeArray('Al Capone') to equal ['firstName' => 'Al', 'lastName' => 'Capone']"
+    //     );
 
-        $this->assertEquals(
-            ['firstName' => 'John', 'lastName' => 'Dillinger'],
-            convertNameToAssociativeArray('John Dillinger'),
-            "Expected convertNameToAssociativeArray('John Dillinger') to equal ['firstName' => 'John', 'lastName' => 'Dillinger']"
-        );
-    }
+    //     $this->assertEquals(
+    //         ['firstName' => 'John', 'lastName' => 'Dillinger'],
+    //         convertNameToAssociativeArray('John Dillinger'),
+    //         "Expected convertNameToAssociativeArray('John Dillinger') to equal ['firstName' => 'John', 'lastName' => 'Dillinger']"
+    //     );
+    // }
 
-    public function test_five_to()
-    {
-        $this->assertInternalType(
-            'callable',
-            'fiveTo',
-            "Expected fiveTo() function to exist."
-        );
+    // public function test_five_to()
+    // {
+    //     $this->assertInternalType(
+    //         'callable',
+    //         'fiveTo',
+    //         "Expected fiveTo() function to exist."
+    //     );
 
-        $this->assertInternalType(
-            'array',
-            fiveTo(10),
-            "Expected fiveTo() function to return an array."
-        );
+    //     $this->assertInternalType(
+    //         'array',
+    //         fiveTo(10),
+    //         "Expected fiveTo() function to return an array."
+    //     );
 
-        $this->assertEquals(
-            [5], fiveTo(5),
-            "Expected fiveTo(5) to equal [5]"
-        );
+    //     $this->assertEquals(
+    //         [5], fiveTo(5),
+    //         "Expected fiveTo(5) to equal [5]"
+    //     );
 
-        $this->assertEquals(
-            [5, 6], fiveTo(6),
-            "Expected fiveTo(6) to equal the array [5, 6]"
-        );
+    //     $this->assertEquals(
+    //         [5, 6], fiveTo(6),
+    //         "Expected fiveTo(6) to equal the array [5, 6]"
+    //     );
 
-        $this->assertEquals(
-            [5, 4, 3, 2, 1, 0, -1, -2], fiveTo(-2),
-            "Expected fiveTo(-2) to equal [5, 4, 3, 2, 1, 0, -1, -2]"
-        );
+    //     $this->assertEquals(
+    //         [5, 4, 3, 2, 1, 0, -1, -2], fiveTo(-2),
+    //         "Expected fiveTo(-2) to equal [5, 4, 3, 2, 1, 0, -1, -2]"
+    //     );
 
-        $this->assertEquals(
-            [5, 6, 7, 8, 9, 10], fiveTo(10),
-            "Expected fiveTo(10) to equal the array [5, 6, 7, 8, 9, 10]"
-        );
-    }
+    //     $this->assertEquals(
+    //         [5, 6, 7, 8, 9, 10], fiveTo(10),
+    //         "Expected fiveTo(10) to equal the array [5, 6, 7, 8, 9, 10]"
+    //     );
+    // }
 
     public function test_upper_case_last_names()
     {
--- assessment.php
+++ assessment.php
@@ -1,3 +1,88 @@
 <?php
 
-// Write your code here
+
+	function isNegative($input)
+	{
+		if($input < 0){
+			return true;
+		}
+		return false;
+	}
+
+
+	function average($arr)
+	{
+		return array_sum($arr) / count(array_filter($arr));
+	}
+
+
+	function countOdds($arr)
+	{
+		$counter = 0;
+		foreach ($arr as $value) {
+			if($value % 2 !== 0) {
+				$counter += 1;
+			}
+		}
+		return $counter;
+	}
+
+
+	function convertNameToAssociativeArray($str)
+	{
+		$arr = explode(" ", $str);
+		$finalArray = ["firstName" => $arr[0], "lastName" => $arr[1]];		
+		return $finalArray;
+	}
+
+
+	function fiveTo($input)
+	{
+		$array = [];
+		$i = 5;
+		array_push($array, $i);
+		while ($i !== $input){	
+			if ($input > 5) {
+				$i++;
+				array_push($array, $i);
+			} elseif ($input < 5){
+				$i--;
+				array_push($array, $i);
+			} 
+		}
+		return $array;
+		}
+
+
+// 1. Write a function named `upperCaseLastNames` that accepts an array of associative arrays
+//    where each associative array has indices `firstName` and `lastName` and returns the
+//    same array of associative arrays with each last name capitalized
+
+        // $names = [
+        //     ['firstName' => 'Harry', 'lastName' => 'Potter'],
+        //     ['firstName' => 'Ron', 'lastName' => 'weasley'],
+        //     ['firstName' => 'Hermione', 'lastName' => 'granger'],
+        // ];
+//         upperCaseLastNames(names);
+        
+//         [
+//             ['firstName' => 'Harry', 'lastName' => 'Potter'],
+//             ['firstName' => 'Ron', 'lastName' => 'Weasley'],
+//             ['firstName' => 'Hermione', 'lastName' => 'Granger'],
+//         ]
+
+	function upperCaseLastNames($aArray)
+	{
+
+	foreach ($aArray as $value) {
+		foreach($value as $key => &$val2){
+		$val2[0] = strtoupper($val2[0]);
+		echo $val2[0];		
+		}
+	}
+}
+
+	// upperCaseLastNames($names);
+
+
+         
