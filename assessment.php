f6aa03822a8786a6674f456e017d92117c4136c1
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-07-28 10:55:40 -0500

    Worked on PHP assessment

--- assessment.php
+++ assessment.php
@@ -1,3 +1,98 @@
 <?php
+// Write a function named isNegative that accepts a number and returns true or false based on whether the input is negative.
 
-// Write your code here
+function isNegative($number){
+	if ($number <0){
+		return is_numeric($number);
+	} else {
+		return is_numeric($number);
+	}
+}
+// isNegative(-1);
+// isNegative(6);
+
+ // isNegative(-1) // true
+ // isNegative(6) // false
+// Write a function named average that accepts an array of numbers and returns the 
+// average of those numbers
+function average($array){
+	$sum = array_sum($array);
+	$result = $sum/count($array);
+	return $result;
+	}
+// average([1, 2, 3]);
+//  average([1, 2, 3]) // 2
+//  average([4, 6, 8, 10, 12]) // 8
+
+// Write a function named countOdds that accepts an array of numbers and returns the 
+// number of odd numbers in the array.
+
+function countOdds($array){
+	$oddNumbers = [];
+	foreach($array as $number){
+		if ($number %2 !=0){
+			array_push($oddNumbers, $number);
+		}
+	}
+		return count($oddNumbers,1);
+}
+
+// var_dump(countOdds([1, 2, 3]));
+// var_dump(countOdds([4, 6, 8, 10]));
+// var_dump(countOdds([1, 2, 1, 1]));
+
+//  countOdds([1, 2, 3]) // 2
+//  countOdds([4, 6, 8, 10]) // 0
+//  countOdds([1, 2, 1, 1]) // 3
+
+// Write a function named convertNameToAssociativeArray that accepts a string that 
+// contains a first name and last name separated by a space, and returns an associative 
+// array with keys of firstName and lastName.
+function convertNameToAssociativeArray($string){
+	$newAssociativeArray = [];
+	$name = explode(' ', $string);
+	$associativeArray['firstName'] = $name[0];
+	$associativeArray['lastName'] = $name[1];
+	array_push($newAssociativeArray,$associativeArray);
+	
+	return $associativeArray;
+}
+//  convertNameToAssociativeArray('Harry Potter') // ['firstName' => 'Harry', 'lastName' => 'Potter']
+//  convertNameToAssociativeArray('Ron Weasley') // ['firstName' => 'Ron', 'lastName' => 'Weasley']
+
+// Write a function named fiveTo that accepts a number and returns an array of the integers 
+// from 5 to (including) the passed number.
+function fiveTo($number){
+	$array = [];
+	for ($i=5; $i <= $number; $i++){
+		array_push($array, $i);
+	}
+	for($i=5; $i >= $number; $i--){
+		array_push($array, $i);
+	}
+		
+	return $array;
+}
+
+ // fiveTo(10) // [5, 6, 7, 8, 9, 10]
+ // fiveTo(6) // [5, 6]
+ // fiveTo(5) // [5]
+ // fiveTo(-2) // [5, 4, 3, 2, 1, 0, -1, -2]
+
+// Write a function named upperCaseLastNames that accepts an array of associative arrays 
+// where each associative array has indices firstName and lastName and returns the same array 
+// of associative arrays with each last name capitalized
+
+//  $names = [
+//      ['firstName' => 'Harry', 'lastName' => 'Potter'],
+//      ['firstName' => 'Ron', 'lastName' => 'weasley'],
+//      ['firstName' => 'Hermione', 'lastName' => 'granger'],
+//  ];
+//  upperCaseLastNames(names);
+// //  /*
+//  [
+//      ['firstName' => 'Harry', 'lastName' => 'Potter'],
+//      ['firstName' => 'Ron', 'lastName' => 'Weasley'],
+//      ['firstName' => 'Hermione', 'lastName' => 'Granger'],
+//  ]
+?>
