7a6825a5cdb351fd3a2bd64a34024f662a10ec31
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-28 09:27:56 -0500

    Count Odds is done

--- assessment.php
+++ assessment.php
@@ -1,7 +1,6 @@
 <?php
 
 //Negative Checker
-
 function isNegative($num){
 	if ($num < 0){
 		return true;
@@ -10,8 +9,20 @@ function isNegative($num){
 	}
 }
 
+//Average
 function average($arr) {
 	$sum = array_sum($arr);
 	$avg = $sum/count($arr);
 	return $avg;
+}
+
+//Odd counter
+function countOdds($arr){
+	$oddCount = 0;
+	for ($i = 0; $i < count($arr); $i++){
+		if ($arr[$i]%2 != 0){
+			$oddCount++;
+		}
+	}
+	return $oddCount;
 }
